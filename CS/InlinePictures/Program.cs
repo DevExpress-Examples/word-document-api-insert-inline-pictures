743c0c0fc651e01d49b41e52cb630db188df997d
Polina Fedorova <polina.fedorova@devexpress.com>
2023-06-16 12:46:21 +0400

    update code

--- CS/InlinePictures/Program.cs
+++ CS/InlinePictures/Program.cs
@@ -1,68 +1,50 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-#region #usings
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using DevExpress.XtraRichEdit;
 using DevExpress.XtraRichEdit.API.Native;
-#endregion #usings
 
-namespace InlinePictures {
-    static class Program {
+namespace InlinePictures
+{
+    static class Program
+    {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            #region #inlinepictures
-            RichEditDocumentServer server = new RichEditDocumentServer();
-            server.LoadDocument("Texts\\InlinePictures.rtf", DocumentFormat.Rtf);
-            Document doc = server.Document;
-            
-            // Insert an image from a file.
-            DocumentRange rangeFound = doc.FindAll("Visual Studio Magazine", SearchOptions.CaseSensitive)[0];
-            DocumentPosition pos = doc.Paragraphs[doc.Paragraphs.Get(rangeFound.End).Index + 2].Range.Start;
-            doc.Images.Insert(pos, DocumentImageSource.FromFile("Pictures\\ReadersChoice.png"));
-            
-            // Insert an image from a stream.
-            pos = doc.Paragraphs[4].Range.Start;
-            string imageToInsert = "information.png";
-            Assembly a = Assembly.GetExecutingAssembly();
-            Stream imageStream = a.GetManifestResourceStream("InlinePictures.Resources." + imageToInsert);
-            doc.Images.Insert(pos, DocumentImageSource.FromStream(imageStream));
-            
-            // Insert an image using its URI.
-            string imageUri = "http://i.gyazo.com/798a2ed48a3535c6c8add0ea7a4fc4e6.png";
-            SubDocument docHeader = doc.Sections[0].BeginUpdateHeader();
-            docHeader.Images.Append(DocumentImageSource.FromUri(imageUri, server));
-            doc.Sections[0].EndUpdateHeader(docHeader);
-
-
-            // Insert a barcode.
-            DevExpress.BarCodes.BarCode barCode = new DevExpress.BarCodes.BarCode();
-            barCode.Symbology = DevExpress.BarCodes.Symbology.QRCode;
-            barCode.CodeText = "http://www.devexpress.com";
-            barCode.CodeBinaryData = System.Text.Encoding.Default.GetBytes(barCode.CodeText);
-            barCode.Module = 0.5;
-            SubDocument docFooter = doc.Sections[0].BeginUpdateFooter();
-            docFooter.Images.Append(DocumentImageSource.FromImage(barCode.BarCodeImage));
-            doc.Sections[0].EndUpdateFooter(docFooter);
-            #endregion #inlinepictures
-
-            #region #getimages
-            // Scale down images in the document body.
-           ReadOnlyDocumentImageCollection images = server.Document.Images.Get(doc.Range);
-            for (int i = 0; i < images.Count; i++)
+            using (RichEditDocumentServer wordProcessor = new RichEditDocumentServer())
             {
-                images[i].ScaleX /= 4;
-                images[i].ScaleY /= 4;
+                wordProcessor.LoadDocument("Texts\\InlinePictures.rtf", DocumentFormat.Rtf);
+                Document document = wordProcessor.Document;
+
+                // Insert an image from a file.
+                DocumentRange rangeFound = document.FindAll("Visual Studio Magazine", SearchOptions.CaseSensitive)[0];
+                DocumentPosition pos = document.Paragraphs[document.Paragraphs.Get(rangeFound.End).Index + 2].Range.Start;
+                Shape imageFromFile = document.Shapes.InsertPicture(pos, DocumentImageSource.FromFile("Pictures\\ReadersChoice.png"));
+                imageFromFile.TextWrapping = TextWrappingType.InLineWithText;
+
+                // Insert an image from a stream.
+                pos = document.Paragraphs[4].Range.Start;
+                string imageToInsert = "information.png";
+                Assembly a = Assembly.GetExecutingAssembly();
+                Stream imageStream = a.GetManifestResourceStream("InlinePictures.Resources." + imageToInsert);
+               Shape imageFromStream =  document.Shapes.InsertPicture(pos, DocumentImageSource.FromStream(imageStream));
+                imageFromStream.TextWrapping = TextWrappingType.InLineWithText;
+
+                // Insert an image using its URI.
+                string imageUri = "http://i.gyazo.com/798a2ed48a3535c6c8add0ea7a4fc4e6.png";
+                SubDocument docHeader = document.Sections[0].BeginUpdateHeader();
+                Shape headerImage = docHeader.Shapes.InsertPicture(docHeader.Range.End, DocumentImageSource.FromUri(imageUri, wordProcessor));
+                headerImage.TextWrapping = TextWrappingType.InLineWithText;
+
+                // Save the resulting document.
+                wordProcessor.SaveDocument("InlinePictures.docx", DocumentFormat.OpenXml);
             }
-            #endregion #getimages
-            // Save the resulting document.
-            server.SaveDocument("InlinePictures.docx", DocumentFormat.OpenXml);
-            Process.Start("InlinePictures.docx");            
+                Process.Start("InlinePictures.docx");
         }
     }
 }
--- Readme.md
+++ Readme.md
@@ -3,12 +3,20 @@
 [![](https://img.shields.io/badge/Open_in_DevExpress_Support_Center-FF7200?style=flat-square&logo=DevExpress&logoColor=white)](https://supportcenter.devexpress.com/ticket/details/E4803)
 [![](https://img.shields.io/badge/📖_How_to_use_DevExpress_Examples-e9f6fc?style=flat-square)](https://docs.devexpress.com/GeneralInformation/403183)
 <!-- default badges end -->
-<!-- default file list -->
-*Files to look at*:
+# Word Processing Document API - Insert Inline Pictures in a Document
+
+This example demonstrates how to use the [Word Processing Document API](https://docs.devexpress.com/OfficeFileAPI/17488/Word-Processing-Document-API) methods to insert inline pictures into a document.
+
+The [Shape API](https://docs.devexpress.com/OfficeFileAPI/15315/word-processing-document-api/word-processing-document/shapes) subset of Word Processing Document API allows you to insert images from file, stream or URI. Use the [DocumentImageSource](https://docs.devexpress.com/OfficeFileAPI/DevExpress.XtraRichEdit.API.Native.DocumentImageSource) class to define the image source.
+
+## Files to Review
 
 * [Program.cs](./CS/InlinePictures/Program.cs) (VB: [Program.vb](./VB/InlinePictures/Program.vb))
-<!-- default file list end -->
-# Word Processing Document API - Insert Inline Pictures in a Document 
 
+## Documentation
 
-This example demonstrates how to use the [Word Processing Document API](https://documentation.devexpress.com/OfficeFileAPI/17488/Word-Processing-Document-API) methods to insert inline pictures into a document.
+* [Shapes, Pictures, and Other Graphic Objects in Word Documents](https://docs.devexpress.com/OfficeFileAPI/15315/word-processing-document-api/word-processing-document/shapes)
+
+## More Examples
+
+* [Word Processing Document API – How to Process Word Documents in Code](https://github.com/DevExpress-Examples/word-document-api-examples)
